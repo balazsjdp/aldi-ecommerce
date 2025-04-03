3dd9450bb93cd8bc45e09407460c633e7619b84a
@hbalazs <balazs.horvath@aldi-sued.com>
2025-04-03 15:28:36 +0200

    To Angular18

--- src/app/pages/product-list/product-list.component.spec.ts
+++ src/app/pages/product-list/product-list.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -9,8 +10,9 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent, HttpClientTestingModule],
-    }).compileComponents();
+    imports: [ProductListComponent],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+}).compileComponents();
 
     fixture = TestBed.createComponent(ProductListComponent);
     component = fixture.componentInstance;
